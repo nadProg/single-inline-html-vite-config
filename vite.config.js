71beb1beb3954647611f108b23efbb4e8424344d
Eugene Skrobov <skrobov.ev@gmail.com>
2024-09-13 19:55:19 +0700

    Fine-tune vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import {defineConfig} from 'vite';
+import {createHtmlPlugin} from 'vite-plugin-html';
 import {viteSingleFile} from 'vite-plugin-singlefile';
-import {createHtmlPlugin} from 'vite-plugin-html'
 
 export default defineConfig({
-    plugins: [viteSingleFile({ removeViteModuleLoader: true}), createHtmlPlugin()],
+  plugins: [
+    viteSingleFile({removeViteModuleLoader: true}),
+    createHtmlPlugin()
+  ],
 })
